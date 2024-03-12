9e6b69c5effdbd1d1defcf4815c3ca3a576f6a73
inowshin19 <isratnowshin.bd@gmail.com>
2024-03-12 09:32:20 +0100

    merged changes from aas fix branch

--- opc2aas/src/main/java/submodel/SubmodelRepositoryComponentForOpc.java
+++ opc2aas/src/main/java/submodel/SubmodelRepositoryComponentForOpc.java
@@ -5,14 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+import org.eclipse.digitaltwin.basyx.submodelrepository.component.SubmodelRepositoryComponent;
 import org.springframework.context.ApplicationContext;
 import org.eclipse.digitaltwin.basyx.submodelrepository.SubmodelRepository;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
 
 @SpringBootApplication(
         scanBasePackages = {"org.eclipse.digitaltwin.basyx", "org.eclipse.digitaltwin.basyx.opc2aas"},
         exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+//@ComponentScan("org.eclipse.digitaltwin.basyx")
 public class SubmodelRepositoryComponentForOpc {
     public static void main(String[] args) throws IOException {
 
